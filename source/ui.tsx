df8af6c9fe15dc3c92002ebae82351db1df4c027
farkmarnum <mark@nayya.com>
2022-05-09 05:41:36 -0400

    Cleanup

--- source/ui.tsx
+++ source/ui.tsx
@@ -1,31 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import { Box } from 'ink';
-import ChooseCommand from './components/ChooseCommand';
+import React, { useEffect } from 'react';
+import { Box, useApp } from 'ink';
 import Setup from './components/Setup';
 import Watch from './components/Watch';
-import { COMMANDS } from './helpers/constants';
-
-const parseCommand = (args: string[]): Command | undefined => {
-  const [command] = args;
-  if (command === undefined || COMMANDS.includes(command as Command)) {
-    return undefined;
-  }
-  return command as Command;
-};
+import { COMMANDS, PACKAGE_NAME } from './helpers/constants';
 
 const App: React.FC<{ args: string[] }> = ({ args }) => {
-  const [command, setCommand] = useState<Command | undefined>();
+  const [command] = args;
+
+  const { exit } = useApp();
 
   useEffect(() => {
-    const commandFromArgs = parseCommand(args);
-    if (commandFromArgs) {
-      setCommand(commandFromArgs);
+    if (command === undefined || !COMMANDS.includes(command as Command)) {
+      console.error(
+        [
+          `USAGE: npx ${PACKAGE_NAME} [command]`,
+          `Try npx ${PACKAGE_NAME} --help`,
+        ].join('\n\n'),
+      );
+      exit();
     }
   }, []);
 
   return (
     <Box>
-      {command === undefined && <ChooseCommand setCommand={setCommand} />}
       {command === 'setup' && <Setup />}
       {command === 'watch' && <Watch />}
     </Box>
