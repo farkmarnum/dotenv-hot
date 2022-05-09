053523fff963a86fe6bb471790d4689145254903
farkmarnum <mark@nayya.com>
2022-05-09 05:34:19 -0400

    Cleanup

--- source/helpers/util.ts
+++ source/helpers/util.ts
@@ -1,9 +1,3 @@
 import fs from 'fs';
-import chalk from 'chalk';
-
-export const quit = (msg: string) => {
-  console.error(`${chalk.red.bold('ERROR:')} ${msg}\n`);
-  process.exit(1);
-};
 
 export const isTypescript = () => fs.existsSync('tsconfig.json');
